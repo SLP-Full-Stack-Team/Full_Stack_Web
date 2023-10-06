aa75c109c77782e54349cc253269740db99a2b88
Chia Yuan Wu <cpm2013317@gmail.com>
2023-10-06 16:06:45 -0700

    100623

--- she_learns/src/pages/Upload/Upload.js
+++ she_learns/src/pages/Upload/Upload.js
@@ -1,22 +1,23 @@
 import './Upload.css';
+import * as React from 'react';
 
 function Upload() {
   return (
     <div className="Upload">
-      <header className="Upload-header">
-        <img src= 'sheCodes_logo.png' className="Upload-logo" alt="logo" />
-        <p>Upload Page</p>
-        <p>
-          Edit <code>src/UploadPage/Upload.js</code> and save to reload.
-        </p>
-        <a
-          className="Upload-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <body>
+            <div>
+                 <p>Upload Page</p>
+                        <a
+                          className="Upload-link"
+                          href="https://reactjs.org"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          Learn React
+                        </a>
+            </div>
+        </body>
       </header>
     </div>
   );
