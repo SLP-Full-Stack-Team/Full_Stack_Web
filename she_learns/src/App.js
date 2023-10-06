5d9987e5e6cc45026e98cfb65ea1504a44b7e867
Min <minjung10402@gmail.com>
2023-10-06 14:49:10 -0700

    adding upload and user page

--- she_learns/src/App.js
+++ she_learns/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Main from './pages/Main/Main'
 import SignIn from './pages/SignIn/SignIn';
 import SignUp from './pages/CreateUser/CreateUser'
+import User from './pages/User/User'
+import Upload from './pages/Upload/Upload'
 
 
 function App() {
@@ -12,12 +14,16 @@ function App() {
             <Link to='/main' className='nav-link-main'><img src= 'sheCodes_logo.png' className='nav-logo'></img>sheLearns</Link>
             <Link to='/signin' className='nav-link-signin'>Sign In</Link>
             <Link to='/signup' className='nav-link-signup'>Sign Up</Link>
+            <Link to='/user' className='nav-link-user'>User</Link>
+            <Link to='/upload' className='nav-link-upload'>Upload</Link>
         </nav>
-        
+
         <Routes>
             <Route path='/main' element={<Main />} />
             <Route path='/signin' element={<SignIn />} />
             <Route path='/signup' element={<SignUp />} />
+            <Route path='/user' element={<User />} />
+            <Route path='/upload' element={<Upload />} />
         </Routes>
     </Router>
   );
