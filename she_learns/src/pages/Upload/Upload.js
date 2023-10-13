321ca8764551825f41f6af9fc6596e6047d437e5
Chia Yuan Wu <cpm2013317@gmail.com>
2023-10-13 15:33:52 -0700

    type something

--- she_learns/src/pages/Upload/Upload.js
+++ she_learns/src/pages/Upload/Upload.js
@@ -8,14 +8,20 @@ function Upload() {
         <body>
             <div>
                  <p>Upload Page</p>
-                        <a
-                          className="Upload-link"
+                        <a className="Upload-link"
                           href="https://reactjs.org"
                           target="_blank"
                           rel="noopener noreferrer"
                         >
                           Learn React
                         </a>
+            </div>
+            <div>
+                <select>
+                    <option value="Sorted_Array_01">Sorted Array</option>
+                    <option value="Sorted_Array_02">Sorted Array2</option>
+                    <option value="Binary_Trees">Binary Trees</option>
+                </select>
             </div>
         </body>
       </header>
