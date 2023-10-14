5a4554c4c8530417e96c58c4eed1527b54ea5b86
Christy Xiong <xiongchristy@gmail.com>
2023-10-13 20:14:45 -0700

    frontend finished

--- she_learns/src/pages/User/User.js
+++ she_learns/src/pages/User/User.js
@@ -1,22 +1,39 @@
 import './User.css';
+import userPic from './images/profile-user.png'
 
 function User() {
   return (
     <div className="User">
       <header className="User-header">
-        <img src= 'sheCodes_logo.png' className="User-logo" alt="logo" />
-        <p>User Page</p>
-        <p>
-          Edit <code>src/UserPage/User.js</code> and save to reload.
-        </p>
-        <a
-          className="User-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="User-profile">
+          <div className = "User-container">
+            <div className = "User-items">
+              <div className = "User-info">
+                <div className = "User-pic">
+                  <img className="picture" src={userPic} alt = "user profile picture"  />
+                </div>
+                <div className = "User-bio">
+                  <body className = "User-bio-item" id = "User-name" >Jane Cooper</body>
+                  <body className = "User-bio-item"  id = "User-detail" >jane123</body>
+                  <body className = "User-bio-item"  id = "User-detail" >jane123@gmail.com</body>
+                </div>
+              </div>
+            </div>
+            <div className = "User-items" id = 'User-upload-area'>
+              <div className="User-upload-header">
+                <div className = "User-upload-title">Upload History</div>
+                <div className = "User-upload-btn-area">
+                  <button className = "User-upload-btn">UPLOAD</button>
+                </div>
+              </div>
+              <div className="User-uploads">
+                <div className = "User-uploads-details">
+                No Uploads Yet...
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
