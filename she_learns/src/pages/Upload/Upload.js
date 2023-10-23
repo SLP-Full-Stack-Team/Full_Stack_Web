626f4270c30989110982c4b2d3f01d677cdd93dc
Chia Yuan Wu <cpm2013317@gmail.com>
2023-10-22 18:28:27 -0700

    102023II

--- she_learns/src/pages/Upload/Upload.js
+++ she_learns/src/pages/Upload/Upload.js
@@ -1,7 +1,14 @@
 import './Upload.css';
 import * as React from 'react';
-
+import { Link } from 'react-router-dom';
+//need to install npm install react-router-dom
 function Upload() {
+    const [d_type, set_Data] = React.useState('');
+    const data_type = [
+    {name:'.pdf',id:'pdf', d_type:'pdf'},
+    {name:'.jpg', id:'jpg', d_type:'jpg'},
+    {name:'youtube', id:'url', d_type:'url'}
+    ];
     const topic = [
     {label:'Sorted Array 01', value:'Sorted Array1'},
     {label:'Sorted Array 02', value:'Sorted Array'},
@@ -18,22 +25,45 @@ function Upload() {
         <body>
             <div>
                 <Dropdown
-                label="Question Topic"
+                label=""
                 options={topic}
                 value={value}
                 placeholder="Select a game"
                 onChange={handleChange}
                 />
-
             </div>
+             <div className="custom-dropdown">
+             {
+             data_type.map((option) => (
+                <label key={option.d_type}>
+                <input
+                    type="radio"
+                    value={option.d_type}
+                    id={option.id}
+                    checked={value === option.d_type}
+                    onChange={handleChange}
+                />
+                {option.name}
+                </label>
+              ))
+              }
+             </div>
+             <div>
+                <Link to="/path-to-another-page">
+                    <button className="rect-btn">Next
+                    </button>
+                </Link>
+             </div>
         </body>
       </header>
     </div>
   );
 }
+const handleButtonClick = () => {
+
+}
 const Dropdown = ({label, value, options, placeholder, onChange}) => {
  return (
-
    <label>
      {label}
      <select value={value} onChange={onChange}>
