d92e9e7b01f938b29d4e9d16ae57f0a98b252b52
mytam201 <tamdinh788@gmail.com>
2023-11-16 17:13:57 -0800

    update

--- she_learns/src/pages/SignIn/SignIn.js
+++ she_learns/src/pages/SignIn/SignIn.js
@@ -1,37 +1,88 @@
+import { Fragment, useState } from 'react';
 import './SignIn.css';
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function SignIn() {
-  const [inputs, setInputs] = useState({});
+const SignIn = () => {
+  const [inputs, setInputs] = useState({
+    user_name: '',
+    user_pswd: '',
+  });
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputs((prevInputs) => ({
+      ...prevInputs,
+      [name]: value,
+    }));
+  };
+
+  const onSubmitSignInForm = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:5002/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(inputs),
+      });
+
+      console.log(response);
+
+      if (response.ok) {
+        // HTTP status code 200-299 indicates success
+        window.location = '/main';
+      } else if (response.status === 401) {
+        // Invalid username or password
+        setErrorMessage('Invalid username or password');
+      } else {
+        // Handle other errors
+        console.error('Error during login');
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
   return (
-    <div className="SignIn">
-      <body className="SignIn-body">
-        <h2>Sign In</h2>
-        <form className="si-form-input">
-          <label>Username</label>
-          <input 
-            className="si-input-box" 
-            type="text"></input>
-        </form>
-        <form className="si-form-input">
-          <label>Password</label>
-          <input 
-            className="si-input-box" 
-            type="text"></input>
-        </form>
-     
-        <Link to='/main' className='btn si-submit-button '>Sign In</Link>
-       
-        <div className="si-not-user-link">
-          <p>Not a user?</p>
-          <Link to ='/signup'>Create an Account</Link>
-        </div>
-        
-        
-      </body>
-    </div>
+    <Fragment>
+      <div className="SignIn">
+        <body className="SignIn-body">
+          <h2>Sign In</h2>
+          <form className="si-form-input">
+            <label>Username</label>
+            <input
+              className="si-input-box"
+              type="text"
+              name="user_name"
+              value={inputs.user_name}
+              onChange={handleInputChange}
+            />
+          </form>
+          <form className="si-form-input">
+            <label>Password</label>
+            <input
+              className="si-input-box"
+              type="password"
+              name="user_pswd"
+              value={inputs.user_pswd}
+              onChange={handleInputChange}
+            />
+          </form>
+
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+          <button className="btn si-submit-button" onClick={onSubmitSignInForm}>
+            Sign In
+          </button>
+
+          <div className="si-not-user-link">
+            <p>Not a user?</p>
+            <Link to="/signup">Create an Account</Link>
+          </div>
+        </body>
+      </div>
+    </Fragment>
   );
-  }
+};
 
 export default SignIn;
